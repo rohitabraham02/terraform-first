09f9249cf68c5abf36f09fb9de403733b887d29f
rohitabraham02 <rohit.abraham02@gmail.com>
2021-05-25 11:24:31 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.59.0"
+    }
+  }
+}
+
+
+provider "azurerm" {
+  features {}
+  
+
+}
+
+
+resource "azurerm_resource_group" "example" {
+  provider = azurerm.other_subscriber
+  for_each = var.name
+  name     = each.key
+  location = "West Europe"
+  tags = {
+    environment = "Dev"
+    project   = "MyLab"
+    owner     = "rohit"
+    newowner  = "newrohit" 
+   }
+  lifecycle {
+    
+  }
+}
